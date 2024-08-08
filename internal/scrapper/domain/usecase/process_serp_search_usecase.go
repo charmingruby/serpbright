a721a0707a43b448acd6aaa3464757c400ccbcad
charmingruby <mrrubycanal3105@gmail.com>
2024-08-08 10:00:55 -0300

    feat: removes processor logic

--- internal/scrapper/domain/usecase/process_serp_search_usecase.go
+++ internal/scrapper/domain/usecase/process_serp_search_usecase.go
@@ -1,23 +1,13 @@
 package usecase
 
 import (
-	"errors"
-
 	"github.com/charmingruby/serpright/internal/scrapper/domain/dto"
-	"github.com/charmingruby/serpright/internal/scrapper/domain/entity/process_entity"
 )
 
 func (s *ScrapperUseCaseRegistry) ProcessSerpSearchUseCase(input dto.ProcessSerpSearchInputDTO) (dto.ProcessSerpSearchOutputDTO, error) {
-	rawData, err := s.Serp.Search(input.CampaignTask)
+	searchResult, err := s.Serp.Search(input.CampaignTask)
 	if err != nil {
 		return dto.ProcessSerpSearchOutputDTO{}, err
-	}
-
-	processor := process_entity.NewSearchProcessor(rawData, input.CampaignTask)
-
-	processedResult, err := processor.ProcessData()
-	if err != nil {
-		return dto.ProcessSerpSearchOutputDTO{}, errors.New("Serp data process error: " + err.Error())
 	}
 
 	//	if err := s.SearchResultRepository.Store(processedResult); err != nil {
@@ -25,6 +15,6 @@ func (s *ScrapperUseCaseRegistry) ProcessSerpSearchUseCase(input dto.ProcessSerp
 	//}
 
 	return dto.ProcessSerpSearchOutputDTO{
-		SearchResult: processedResult,
+		SearchResult: searchResult,
 	}, nil
 }
