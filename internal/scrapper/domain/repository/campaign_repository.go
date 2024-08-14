0e09758e595d648816f7c980eef2fabe06ec61ef
charmingruby <mrrubycanal3105@gmail.com>
2024-08-14 16:27:21 -0300

    feat: removes unused campaign stuff

--- internal/scrapper/domain/repository/campaign_repository.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "github.com/charmingruby/serpright/internal/scrapper/domain/entity"
-
-type CampaignTaskRepository interface {
-	FindByID(id string) (*entity.CampaignTask, error)
-}
--- test/factory/make_campaign_task.go
+++
@@ -1,9 +0,0 @@
-package factory
-
-import "github.com/charmingruby/serpright/internal/scrapper/domain/entity"
-
-func MakeCampaignTask() entity.CampaignTask {
-	return entity.CampaignTask{
-		ID: "Campaign ID",
-	}
-}
