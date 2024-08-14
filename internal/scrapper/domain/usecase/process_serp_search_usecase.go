c12ff980ad96ad16b82edb08e3ac6ef11696898c
charmingruby <mrrubycanal3105@gmail.com>
2024-08-14 09:13:49 -0300

    feat: adds debug of processing result

--- internal/scrapper/domain/usecase/process_serp_search_usecase.go
+++ internal/scrapper/domain/usecase/process_serp_search_usecase.go
@@ -1,24 +1,11 @@
 package usecase
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"time"
-
 	"github.com/charmingruby/serpright/internal/scrapper/domain/dto"
 )
 
 func (s *ScrapperUseCaseRegistry) ProcessSerpSearchUseCase(input dto.ProcessSerpSearchInputDTO) (dto.ProcessSerpSearchOutputDTO, error) {
 	searchResult, err := s.Serp.Search(input.CampaignTask)
-	if err != nil {
-		return dto.ProcessSerpSearchOutputDTO{}, err
-	}
-
-	body, _ := json.Marshal(searchResult)
-
-	path := fmt.Sprintf("./tmp/bright_data_%s_response.json", time.Time.Format(time.Now(), "2006-01-02 15:04:05"))
-	err = os.WriteFile(path, body, 0644)
 	if err != nil {
 		return dto.ProcessSerpSearchOutputDTO{}, err
 	}
--- internal/scrapper/infra/queue/campaign_task_process_handler.go
+++ internal/scrapper/infra/queue/campaign_task_process_handler.go
@@ -2,7 +2,11 @@ package queue
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"log/slog"
+	"os"
+	"time"
 
 	"github.com/charmingruby/serpright/internal/scrapper/domain/dto"
 	"github.com/charmingruby/serpright/internal/scrapper/domain/entity"
@@ -34,6 +38,16 @@ func (h *CampaignTaskProcessHandler) Handle(msg []byte) {
 	output, err := h.ScrapperService.ProcessSerpSearchUseCase(input)
 	if err != nil {
 		log.Fatal(err.Error())
+	}
+
+	body, err := json.Marshal(output.SearchResult)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	path := fmt.Sprintf("./tmp/search_result_%s_response.json", time.Time.Format(time.Now(), "2006-01-02 15:04:05"))
+	if err := os.WriteFile(path, body, 0644); err != nil {
+		slog.Error(err.Error())
 	}
 
 	log.Printf("TASK PROCESSED: %v", output.SearchResult.Task.BrandName)
